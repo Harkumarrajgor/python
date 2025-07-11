e59fc58173c87791aaeaf41a72742164cf37feea
Harkumarrajgor <harrajgor88@gmail.com>
2025-07-11 15:02:42 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,20 @@
+#1) how to the python code structure work?
+x=10
+y=20
+z=x+y
+print(z)
+
+
+#2) how to create variable in python?
+a=10
+b="Har"
+c=10.8
+is_valid=True
+print(a)
+print(b)
+print(c)
+
+
+#3) how to take input from users?
+a=int(input("Enter a number")) #int keyword is use to convert string into integer
+print(a)
