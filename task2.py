4fd8c09ab6f9026369e5abeee2bfa88cec8b80a9
Harkumarrajgor <harrajgor88@gmail.com>
2025-07-15 16:00:15 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,27 @@
+#4) how to check the variable dynamically?
+a=10
+b="Har"
+c=10.8
+is_valid=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(is_valid))
+
+#5) W.A.P to find greater and less than number using if_else
+a=10
+b=20
+if(a>=b):
+    print("A is greater")
+    print("B is smaller")
+else:
+    print("B is greater")
+    print("A is smaller")
+
+
+#6) W.A.P to find prime number using if_else
+a=int(input("Enter a number"))
+if(a%a):
+    print("It is not a prime number")
+else:
+    print("It is a prime number")
