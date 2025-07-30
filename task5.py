85f3d5b22dbb9a0a216999413c2ab2288b8439ff
Harkumarrajgor <harrajgor88@gmail.com>
2025-07-30 18:19:03 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,20 @@
+#5) W.A.P to skip the (banana) from the list using control statement
+one=["apple","banana","mango"]
+for i in one:
+    if(i=="banana"):
+        continue
+    print(i)
+
+#6) W.A.P to break the for loop when (banana) get in if condition 
+one=["apple","banana","mango"]
+for i in one:
+    if(i=="banana"):
+        break
+    print(i)
+
+#1) W.A.P to print(Hello) using string
+print("Hello")
+
+#2) W.A.P to allocate the string to a variable
+a="python"
+print(a)
