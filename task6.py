6c94aba1d115046495d1f52e1d0fb53203557b23
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-01 22:36:14 +0530

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,15 @@
+#3) W.A.P to print string using three quotes
+a="""python allow to write the codes in the multiline
+using the three quotes functionality. In this functionality
+you can declare a pragaph as a string"""
+
+print(a)
+
+#4) W.A.P to access the 1st position of character using index value
+a="python"
+print("Your first letter is:",a[:1])
+
+#5) W.A.P to access the string after the index value 1
+a="python"
+print("Your remaining string is:",a[1:])
+
