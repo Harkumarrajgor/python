bca3f072e72be37b20ae9118dcc8335bf58022bc
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-07 18:40:08 +0530

    Add files via upload

---
+++ task8.py
@@ -0,0 +1,15 @@
+# Level 2 Questions
+
+#1) W.A.P to create a list of multiple datatype element
+one=[10,20,"apple","banana",20.8,10.5]
+print(one)
+
+#2) W.A.P to find the length of the list 
+one=["apple","banana","mango"]
+print(len(one))
+
+#3) W.A.P to update the list insert() and append()
+one=["apple","banana","mango"]
+one.insert(2,"cherry")
+one.append("lichi")
+print(one)
