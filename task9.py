f15484d1e48247d03c4bff68a9a43138371b820b
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-11 21:20:16 +0530

    Add files via upload

---
+++ task9.py
@@ -0,0 +1,15 @@
+#4) W.A.P to remove the element using the pop() and remove()
+one=["apple","banana","mango"]
+one.remove("banana")
+one.pop()
+print(one)
+
+#Level 2.1 questions
+
+#1) W.A.P to access the value on index value in the list
+one=["apple","banana","mango"]
+print(one[1])
+
+#2) W.A.P to access the value after the index value 1
+first=[10,20,30,40,50,60]
+print("Your value is:",first[1])
