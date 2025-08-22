1810f761e87457c2216fd69b88726e8db186e45a
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-22 23:50:42 +0530

    Add files via upload

---
+++ task11.py
@@ -0,0 +1,17 @@
+#6) W.A.P to irate the list using for loop 
+one=["apple","banana","mango"]
+for i in one:
+    print(i)
+
+#7) W.A.P to insert the value in empty list using for loop and append()
+one=[]
+print(one)
+one.insert(1,"apple")
+print(one)
+one.append("mango")
+print(one)
+
+#8) W.A.P to delete the element using del
+one=["apple","banana","mango"]
+del one[2]
+print(one)
