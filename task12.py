254f8f7a7cb9331502fccf5091ac80f71a50a87c
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-25 23:21:43 +0530

    Add files via upload

---
+++ task12.py
@@ -0,0 +1,18 @@
+#9) W.A.P to sort the list using sort() and sorted()
+first=[20,40,30,10,60,50]
+first.sort()
+print(first)
+first.sort(reverse=True)
+print(first)
+x=sorted(first)
+print(x)
+
+#10) W.A.P to round the value in list using round() and for loop
+first=[10.9865,20.4589,30.5423]
+for i in first:
+    print(round(i,2))
+
+#11) W.A.P to convert the list into tupple
+one=["apple","banana","mango"]
+first=tuple(one)
+print(first)
