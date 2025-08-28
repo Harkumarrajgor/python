50d957d756abd8fa52364b5a7a347c771375b5ec
Harkumarrajgor <harrajgor88@gmail.com>
2025-08-28 23:41:35 +0530

    Add files via upload

---
+++ task13.py
@@ -0,0 +1,13 @@
+#12) W.A.P to create tuple with multiple data type
+two=("banana","mango",10,20,15.8)
+print(two)
+
+#13)  W.A.P to concate the two tuple into one tuple
+one=("apple","banana","mango")
+two=(10,20,30,40,50)
+x=one+two
+print(x)
+
+#14) W.A.P to access the value of index value 1st in tuple
+one=("apple","banana","mango")
+print("Your 1st value is:",one[1])
