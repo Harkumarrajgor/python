b7d0e06fe9f20a553b03bfd27dffd22aa79f0c80
Harkumarrajgor <harrajgor88@gmail.com>
2025-09-03 17:02:40 +0530

    Add files via upload

---
+++ task14.py
@@ -0,0 +1,15 @@
+#15) W.A.P to access the value from last in tuple 
+one=("apple","banana","mango")
+print("Your last value is:",one[-1])
+
+#16) W.A.P to access the value between 1 to 5 from the tuple 
+two=(10,20,30,40,50,60,70)
+print("Value between 1 to 5 is:",two[1:5])
+
+#17) W.A.P to access the alternate value between index 1st to 5th 
+two=(10,20,30,40,50,60,70)
+print("Value between 1st to 5th is:",two[0:5])
+
+#18) W.A.P to create the dictionary having 6 key and value pair 
+new={1:"python",2:"java",3:"c",4:"c++",5:"php",6:"js"}
+print(new)
