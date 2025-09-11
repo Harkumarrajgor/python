554e2f05c5bc10fba6a5d3ee892e67ac8c7380ca
Harkumarrajgor <harrajgor88@gmail.com>
2025-09-11 23:22:06 +0530

    Add files via upload

---
+++ task15.py
@@ -0,0 +1,15 @@
+#19) W.A.P to access the value using the key from dictionary
+new={1:"python",2:"java",3:"php"}
+print(new[2])
+
+#20) W.A.P to update the value on particular key
+new={1:"python",2:"avaj",3:"php"}
+new[2]="java"
+print(new)
+
+#21) W.A.P to seprate the key and value from dictionary using keys() and values() of dictionary
+new={1:"python",2:"java",3:"php"}
+keys=list(new.keys())
+values=list(new.values())
+print(keys)
+print(values)
